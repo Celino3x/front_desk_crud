6d9fad5a0963637733d71c624a978459cedaaeb2
Juscelino Campos <celino3x@gmail.com>
2023-02-01 22:53:03 -0300

    mudança de layout, inclusão de CPF com máscara

--- index.php
+++ index.php
@@ -4,14 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Front Desk | CRUD</title>
 </head>
 <body>
     <section class="container">
     <form>
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Nome</label>
-            <input type="text" class="form-control" id="nome" aria-describedby="nome">
+            <label class="form-label">Nome Completo</label>
+            <input type="text" class="form-control" id="nomeCompleto" aria-describedby="nomeCompleto">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Horário</label>
+            <input type="time" class="form-control" id="horario" aria-describedby="horario">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">CPF</label>
+            <input type="text" class="form-control" oninput="mascara(this)" id="cpf" maxlength="11">
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Senha</label>
@@ -21,5 +30,23 @@
         <button type="submit" class="btn btn-primary">Enviar</button>
     </form>
     </section>
+
+    <script>
+        
+            function mascara(i){
+            
+            var v = i.value;
+            
+            if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+                i.value = v.substring(0, v.length-1);
+                return;
+            }
+            
+            i.setAttribute("maxlength", "14");
+            if (v.length == 3 || v.length == 7) i.value += ".";
+            if (v.length == 11) i.value += "-";
+
+            }
+    </script>
 </body>
 </html>
