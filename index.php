07c7dd5e87ff3a21e68c5a14ec0c5342d2e4dcae
Juscelino Campos <celino3x@gmail.com>
2023-02-01 22:42:10 -0300

    form nome + Senha

--- index.php
+++ index.php
@@ -7,6 +7,19 @@
     <title>Front Desk | CRUD</title>
 </head>
 <body>
-    <section class="container"></section>
+    <section class="container">
+    <form>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Nome</label>
+            <input type="text" class="form-control" id="nome" aria-describedby="nome">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Senha</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+    </section>
 </body>
 </html>
