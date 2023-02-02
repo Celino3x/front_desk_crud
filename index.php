d8ff02e6e88fa02ab813a33eef73a23520daf849
Juscelino Campos <celino3x@gmail.com>
2023-02-02 13:56:18 -0300

    Remodeling the registration page layout

--- index.php
+++ index.php
@@ -4,31 +4,60 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Front Desk | CRUD</title>
 </head>
 <body>
-    <section class="container">
-    <form>
-        <div class="mb-3">
-            <label class="form-label">Nome Completo</label>
-            <input type="text" class="form-control" id="nomeCompleto" aria-describedby="nomeCompleto">
-        </div>
-        <div class="mb-3">
-            <label class="form-label">Horário</label>
-            <input type="time" class="form-control" id="horario" aria-describedby="horario">
-        </div>
-        <div class="mb-3">
-            <label class="form-label">CPF</label>
-            <input type="text" class="form-control" oninput="mascara(this)" id="cpf" maxlength="11">
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Senha</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+    <section class="label container g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sw h-md-250 position-relative">
+        <div>
+            <h3>Cadastro de Visitante</h3>
         </div>
 
-        <button type="submit" class="btn btn-primary">Enviar</button>
+    <form class="form_cad">
+
+        <div class="row">
+            <div class="mb-3 col-md-6">
+                <label class="form-label">Nome Completo</label>
+                <input type="text" class="form-control" id="nomeCompleto" aria-describedby="nomeCompleto">
+            </div>
+
+            <div class="mb-3 col-md-3">
+                <label class="form-label">CPF</label>
+                <input type="text" class="form-control" oninput="mascara(this)" id="cpf" maxlength="11">
+            </div>
+
+            <div class="mb-3 col-md-3">
+                <label class="form-label">RG</label>
+                <input type="number" class="form-control" id="rg">
+            </div>
+
+            <div class="mb-3 col-md-6">
+                <label class="form-label">Empresa</label>
+                <input type="text" class="form-control">
+            </div>
+
+            <div class="mb-3 col-md-6">
+                <label class="form-label">Telefone</label>
+                <input type="number" class="form-control">
+            </div>
+
+            <div class="mb-3">
+                <label class="form-label">Observações</label>
+                <input type="textarea" class="form-control" id="observacao" aria-describedby="observacao">
+            </div>
+
+            <div class="mb-3">
+                <label for="image-upload" id="image-label" class="form-label">Foto</label>
+                <input name="image" id="image-upload" type="file" class="form-control" id="observacao" aria-describedby="observacao">
+            </div>
+
+            <hr>
+
+            <button type="submit" class="mb-6 btn btn-dark">Cadastrar</button>
+
+        </div>
+        
     </form>
     </section>
 
