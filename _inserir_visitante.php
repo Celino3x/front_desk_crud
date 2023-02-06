712c4f2d55f900a0774a6945281638001c8c05c2
Juscelino Campos <celino3x@gmail.com>
2023-02-06 01:21:54 -0300

    inclusion of metadata that is received from cad_visitante and handling for cad_visitante table

--- _inserir_visitante.php
+++ _inserir_visitante.php
@@ -13,20 +13,15 @@ $foto_visitante = $_POST['foto_visitante'];
 
 //upload de arquivo
 
-$msg = false;
+$extensao = strtolower(substr($_FILES['foto_visitante']['name'], -4));
+$new_name = md5(time()) . $extensao;
+$diretorio = "fotos_visitantes/";
 
-if(isset($_FILES['foto_visitante'])){
-    $extensao = strtolower(substr($_FILES['foto_visitante']['name'], -4));
-    $new_name = md5(time()) . $extensao;
-    $diretorio = "fotos_visitantes/";
+move_uploaded_file($_FILES['foto_visitante']['tmp_name'], $diretorio.$new_name);
 
-    move_uploaded_file($_FILES['foto_visitante']['tmp_name'], $diretorio.$new_name);
+$sql = "INSERT INTO `cad_visitante`( `nome_visitante`, `cpf_visitante`, `rg_visitante`, `empresa_visitante`, `telefone_visitante`, `obs_visitante`, `foto_visitante`) VALUES ('$nome_visitante', '$cpf_visitante', '$rg_visitante', '$empresa_visitante', '$telefone_visitante', '$obs_visitante', '$foto_visitante')";
 
-    $sql = "INSERT INTO `cad_visitante`(`nome_visitante`,`cpf_visitante`, `rg_visitante`, `empresa_visitante`, `telefone_visitante`, `foto_visitante`) VALUE ('$nome_visitante', '$cpf_visitante', '$rg_visitante', '$empresa_visitante', '$telefone_visitante', '$obs_visitante', '$foto_visitante')";
-
-     $inserir = mysqli_query($conexao, $sql);
-
-}
+$inserir = mysqli_query($conexao, $sql);
 
 ?>
 
@@ -35,3 +30,4 @@ if(isset($_FILES['foto_visitante'])){
     <a href="Voltar" class="btn btn-sm btn-warning">Voltar</a>
     <a href="Consultar" class="btn btn-sm brn-secondary">Consultar</a>
     </div>
+<?php
