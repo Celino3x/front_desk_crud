265cfadbcc46be277bcfd12c6862cd698cbb605d
Juscelino Campos <celino3x@gmail.com>
2023-02-09 10:36:04 -0300

    Alterações na estilização da página _inserir

--- _inserir_visitante.php
+++ _inserir_visitante.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Front Desk | CRUD</title>
+</head>
+<body>
+
 <?php
 
 include('conexao.php');
@@ -30,4 +42,9 @@ $inserir = mysqli_query($conexao, $sql);
     <a href="Voltar" class="btn btn-sm btn-warning">Voltar</a>
     <a href="_consultar_visitante.php" class="btn btn-sm brn-secondary">Consultar</a>
     </div>
-<?php
+<?php ?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+</body>
+</html>
