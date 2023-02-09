8f9386a39b68cc8c566c73d95cb69dd2561055f7
Juscelino Campos <celino3x@gmail.com>
2023-02-09 11:09:40 -0300

    Correção no inserir foto, erro apresentava devido POST

--- _inserir_visitante.php
+++ _inserir_visitante.php
@@ -20,7 +20,7 @@ $rg_visitante = $_POST['rg_visitante'];
 $empresa_visitante = $_POST['empresa_visitante'];
 $telefone_visitante = $_POST['telefone_visitante'];
 $obs_visitante = $_POST['obs_visitante'];
-$foto_visitante = $_POST['foto_visitante'];
+//$foto_visitante = $_POST['foto_visitante'];
 
 
 //upload de arquivo
