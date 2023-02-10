7856d45fbfe5d0e523b3e45f5a84fa20640e0f56
Juscelino Campos <celino3x@gmail.com>
2023-02-10 12:06:25 -0300

    alterações na conexao.php

--- conexao.php
+++ conexao.php
@@ -1,14 +1,14 @@
 <?php
-    //$server = "localhost";
-    //$database = "front_desk";
-    //$username = "celino3x";
-    //$pass = "123456";
+    $server = "containers-us-west-194.railway.app";
+    $database = "railway";
+    $username = "root";
+    $pass = "fvhS8vnUP9jJmb8TRAdt";
 
-    //$conexao = mysqli_connect($server, $username, $pass, $database);
+    $conexao = mysqli_connect($server, $username, $pass, $database);
 
 
-    $mysqli = mysqli_init();
-    $mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
-    $mysqli->real_connect($_ENV["us-east.connect.psdb.cloud"], $_ENV["4mon01d2c7rdz5cb1pbr"], $_ENV["pscale_pw_nJMs6RmrRaSToA3dNHOKilZQtBEXQyIIbxi63XGAtqE"], $_ENV["front_desk"]);
-    $mysqli->close();
+    //$mysqli = mysqli_init();
+   // $mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+   // $mysqli->real_connect($_ENV["us-east.connect.psdb.cloud"], $_ENV["4mon01d2c7rdz5cb1pbr"], $_ENV["pscale_pw_nJMs6RmrRaSToA3dNHOKilZQtBEXQyIIbxi63XGAtqE"], $_ENV["front_desk"]);
+    //$mysqli->close();
 ?>
